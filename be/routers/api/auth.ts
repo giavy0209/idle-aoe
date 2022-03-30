bda66fa273a51581fc510941b5231341d6fb99a1
giavy0209 <49144082+giavy0209@users.noreply.github.com>
2022-03-31 01:41:37 +0700

    Update auth.ts

--- be/routers/api/auth.ts
+++ be/routers/api/auth.ts
@@ -13,18 +13,21 @@ router.route('/valid-jwt')
     .get(isAuth,controller.isValidJWT)
 
 async function  create() {
-    for (let index = 0; index < 100; index++) {
-        console.log(index);
+    await controller.create()
+    console.log('done');
+    
+    // for (let index = 0; index < 100; index++) {
+    //     console.log(index);
         
-        await controller.signup({
-            body : {
-                username : `user-${index}`,
-                password : `${index}`,
-                world : '623e0f549c7a54a2c0fc90b0'
-            }
-        } , null)
+    //     await controller.signup({
+    //         body : {
+    //             username : `user-${index}`,
+    //             password : `${index}`,
+    //             world : '623e0f549c7a54a2c0fc90b0'
+    //         }
+    //     } , null)
         
-    }
+    // }
 }
-// create()
+create()
 export default router
