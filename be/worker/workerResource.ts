b5a0f6f01ed5077bf5e386169b1db74ffd662f50
giavy0209 <49144082+giavy0209@users.noreply.github.com>
2022-04-02 01:52:08 +0700

    Update workerResource.ts

--- be/worker/workerResource.ts
+++ be/worker/workerResource.ts
@@ -30,9 +30,6 @@ export default async function workerResource () {
                 bonus = (worldEXP - userEXP) / worldEXP * bonusSpeed
             }
 
-            console.log({bonus,userEXP , worldEXP});
-            
-
             const now = Date.now()
             const diffTime = (now - new Date(resource.lastUpdate).getTime()) / 1000
             const percentDiffTimePerHour = diffTime / secInHour
