2bc9e59a8382dfe84354277fdd1bd294b179997e
giavy0209 <49144082+giavy0209@users.noreply.github.com>
2022-04-12 12:53:20 +0700

    a

--- fe/src/components/BattleSimulation/BattleSimulation.tsx
+++ fe/src/components/BattleSimulation/BattleSimulation.tsx
@@ -1,21 +1,118 @@
+import { faChevronCircleLeft, faChevronCircleRight, faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Modal from "components/Modal";
-import { FC, useState } from "react";
+import { _units } from "constant";
+import { FC, useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { actionChangeBattleSimulation } from "store/actions/battle";
 
-const BattleSimulation : FC = () => {
-    const dispatch = useDispatch()
-    const battleSimulation = useSelector((state: any) => state.battleSimulation)
-    const [CurrentRound, setCurrentRound] = useState<{[key : string] : any}>({})
-    console.log(battleSimulation);
-    
+const Formulation: FC<{ units: any }> = ({ units }) => {
     return (
         <>
-        <Modal show={!!battleSimulation} onClose={()=>dispatch(actionChangeBattleSimulation(null))} >
-            <div className="battle-simulation">
-
+            <div className="row-1">
+                {units?.map(o => {
+                    if (o.unit.range === 1) {
+                        return <div className="unit">
+                            <div className="img">
+                                <img src={o.img} alt="" />
+                            </div>
+                            <div className="info">
+                                <div className="curren">{o.total}</div>
+                            </div>
+                        </div>
+                    }
+                    return null
+                })}
             </div>
-        </Modal>
+        </>
+    )
+}
+
+const BattleSimulation: FC = () => {
+    const dispatch = useDispatch()
+    const battleSimulation = useSelector((state: any) => state.battleSimulation)
+    console.log(battleSimulation);
+
+    const [IsAutoPlay, setIsAutoPlay] = useState(false)
+    const [IndexRound, setIndexRound] = useState(1)
+    const [IndexTurn, setIndexTurn] = useState(0)
+
+    const { TotalRound, attackerUnits, defenderUnits } = useMemo(() => {
+        if (!battleSimulation) return {}
+        return {
+            TotalRound: battleSimulation.rounds.length,
+            attackerUnits: battleSimulation.attackerUnits.sort((a, b) => a.unit.range - b.unit.range).map(o => {
+                return { ...o, img: _units.find(_o => _o.name === o.unit.name)?.img }
+            }),
+            defenderUnits: battleSimulation.defenderUnits.sort((a, b) => a.unit.range - b.unit.range).map(o => {
+                return { ...o, img: _units.find(_o => _o.name === o.unit.name)?.img }
+            }),
+        }
+    }, [battleSimulation])
+
+    const currentRound = useMemo(() => {
+        return battleSimulation?.rounds[IndexRound - 1]
+    }, [IndexRound, battleSimulation])
+
+    const currentTurn = useMemo(() => {
+        console.log(currentRound);
+        
+        const turn = currentRound?.actions[IndexTurn]
+        if (!turn) return {}
+        const findDataAttack = _units.find(_o => _o.name === turn.unitAttack.unit.name)
+        const findDataDefend = _units.find(_o => _o.name === turn.unitDefend.unit.name)
+
+        turn.unitAttack = { ...turn.unitAttack, img: findDataAttack?.img,}
+        turn.unitDefend = { ...turn.unitDefend, img: findDataDefend?.img,}
+        return turn
+    }, [IndexRound, currentRound, defenderUnits])
+    console.log(currentTurn);
+    
+
+    return (
+        <>
+            <Modal show={!!battleSimulation} onClose={() => dispatch(actionChangeBattleSimulation(null))} >
+                {battleSimulation && <div className="battle-simulation">
+                    <div className="button-actions">
+                        <div className="block">
+                            <div className="action">Round <div className="icon"><FontAwesomeIcon icon={faChevronCircleLeft} /></div> </div>
+                            <div className="action">Turn <div className="icon"><FontAwesomeIcon icon={faChevronCircleLeft} /></div> </div>
+                        </div>
+                        <div className="block">
+                            <div onClick={() => setIsAutoPlay(!IsAutoPlay)} className="action">Auto Turn <div className="icon"><FontAwesomeIcon icon={IsAutoPlay ? faPause : faPlay} /></div></div>
+                        </div>
+                        <div className="block">
+
+                            <div className="action">Turn <div className="icon"><FontAwesomeIcon icon={faChevronCircleRight} /></div> </div>
+                            <div className="action">Round <div className="icon"><FontAwesomeIcon icon={faChevronCircleRight} /> </div> </div>
+                        </div>
+                    </div>
+                    <div className="player">
+                        <div className="name">{battleSimulation.attacker.username} ({battleSimulation.attackerCastle.name})</div>
+                        <div className="formulation">
+                            {
+                                currentTurn?.unitAttack?.user._id === battleSimulation.attacker._id ?
+                                    <div className="row">
+                                        <img src={currentTurn?.unitAttack.img} alt="" />
+                                    </div>
+                                    :
+                                    <div className="row">
+                                        <img src={currentTurn?.unitDefend.img} alt="" />
+                                    </div>
+                            }
+                        </div>
+
+                    </div>
+                    <div className="round">Round {IndexRound}/{TotalRound}</div>
+                    <div className="player">
+                        <div className="name">{battleSimulation.defender.username} ({battleSimulation.defenderCastle.name})</div>
+                        <div className="formulation">
+
+                        </div>
+
+                    </div>
+                </div>}
+            </Modal>
         </>
     )
 }
--- fe/src/constant.ts
+++ fe/src/constant.ts
@@ -1,2 +1,91 @@
+import Pikeman from 'assets/images/pikeman.webp'
+import Swordsman from 'assets/images/swordsman.webp'
+import Axeman from 'assets/images/axeman.webp'
+import Maceman from 'assets/images/maceman.webp'
+import ShortbowArcher from 'assets/images/shortbow_archer.webp'
+import LongbowArcher from 'assets/images/longbow_archer.webp'
+import CrossbowArcher from 'assets/images/crossbow_archer.webp'
+
+import Quickwalker from 'assets/images/quickwalker.webp'
+import LightCavalry from 'assets/images/light_cavalry.webp'
+import HeavyCavalry from 'assets/images/heavy_cavalry.webp'
+import Ballistician from 'assets/images/ballistician.webp'
+import Catapult from 'assets/images/catapult.webp'
+import Trebuchet from 'assets/images/trebuchet.webp'
+import Nobleman from 'assets/images/nobleman.webp'
+
 export const DOMAIN = process.env.REACT_APP_DOMAIN as string
 export const WS = process.env.REACT_APP_WS as string
+
+export const _units = [
+    {
+        name: 'Pikeman',
+        img: Pikeman,
+        type: "infantry"
+    },
+    {
+        name: 'Swordsman',
+        img: Swordsman,
+        type: "infantry"
+    },
+    {
+        name: 'Axeman',
+        img: Axeman,
+        type: "infantry"
+    },
+    {
+        name: 'Maceman',
+        img: Maceman,
+        type: "infantry"
+    },
+    {
+        name: 'Shortbow archer',
+        img: ShortbowArcher,
+        type: "archer"
+    },
+    {
+        name: 'Longbow archer',
+        img: LongbowArcher,
+        type: "archer"
+    },
+    {
+        name: 'Crossbow archer',
+        img: CrossbowArcher,
+        type: "archer"
+    },
+    {
+        name: 'Quickwalker',
+        img: Quickwalker,
+        type: "cavalry"
+    },
+    {
+        name: 'Light Cavalry',
+        img: LightCavalry,
+        type: "cavalry"
+    },
+    {
+        name: 'Heavy Cavalry',
+        img: HeavyCavalry,
+        type: "cavalry"
+    },
+    {
+        name: 'Ballistician',
+        img: Ballistician,
+        type: "siege"
+    },
+    {
+        name: 'Catapult',
+        img: Catapult,
+        type: "siege"
+    },
+    {
+        name: 'Trebuchet',
+        img: Trebuchet,
+        type: "siege"
+    },
+    {
+        name: 'Nobleman',
+        img: Nobleman,
+        type: "unique"
+    },
+]
